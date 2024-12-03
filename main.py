9b7fa3cfb12655e903f16db951b66b90ef0e5565
xyzuan <xyzuannihboss@gmail.com>
2024-12-03 10:45:45 +0700

    feat(ci/cd): Create Github Action workflows

--- main.py
+++ main.py
@@ -62,4 +62,4 @@ def process_image():
     return jsonify({"error": "Unknown error occurred"}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
